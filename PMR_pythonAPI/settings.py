7b6808ab46678df720084dfbcf8ce8d52a39371b
valerian <valerianvdc.pro@gmail.com>
2024-03-16 21:26:05 +0100

    add allowed host

--- PMR_pythonAPI/settings.py
+++ PMR_pythonAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=w2+3inx38x5ij!o0n!w_fk-kx_*4$-5q19=+*_9v(zw4ejd)k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pmr-pythonapi.onrender.com"]
 
 
 # Application definition
