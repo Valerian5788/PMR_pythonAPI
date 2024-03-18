01cf39983dd7531cb94f709ad23af2023894e13c
valerian <valerianvdc.pro@gmail.com>
2024-03-18 16:39:29 +0100

    Commencement tri code. Stop Namur correct place

--- pythonAPI_PMR/views.py
+++ pythonAPI_PMR/views.py
@@ -19,42 +19,10 @@ def documentation_Charleroi(request):
     return render(request, 'stop_charleroi.html')
 
 
-def arrets_de_bus_zone_Namur(request):
-    # Coordonnées géographiques de la gare de Namur
-    lat = 50.466667  # Latitude en décimal
-    lon = 4.866667  # Longitude en décimal
-    radius = 0.02  # Rayon de la zone autour de la gare (5 km = 0.05 degré en approximation)
-
-    # Définition de la zone autour de la gare
-    min_lat = lat - radius
-    max_lat = lat + radius
-    min_lon = lon - radius
-    max_lon = lon + radius
-
-    # Construction de l'URL de l'API avec les paramètres de recherche
-    url = "https://www.odwb.be/api/explore/v2.1/catalog/datasets/gtfs_tec_stops/records?select=*&where=stop_name%20like%20%27NAMUR%27&limit=99"
-
-    # Requête à l'API
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        arrets_dans_zone = []
-        for result in data["results"]:
-            if "stop_id" in result and "stop_name" in result and "stop_coordinates" in result:
-                stop_info = {
-                    "stop_id": result["stop_id"],
-                    "stop_name": result["stop_name"],
-                    "stop_coordinates": result["stop_coordinates"]
-                }
-                arrets_dans_zone.append(stop_info)
-
-        # Retourner les données JSON des arrêts dans la zone
-        return JsonResponse({"arret_autour_zone": arrets_dans_zone})
-    else:
-        # Gestion des erreurs
-        return JsonResponse({"error": "Erreur lors de la récupération des données API"}, status=500)
-
+def arret_de_bus_zone_Namur(request):
+    # Call function from bus_stops_logic module to get bus stop data for Namur
+    data = CrowdManagement.get_bus_stops_in_namur()
+    return JsonResponse(data)
 
 def documentation_namur(request):
     return render(request, 'stop_namur.html')
