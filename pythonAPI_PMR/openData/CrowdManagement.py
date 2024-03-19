af84ea1133113956bae52e6527c363d0c0a5d534
valerian <valerianvdc.pro@gmail.com>
2024-03-19 11:01:01 +0100

    repo cleaning, CW charleroi

---
+++ pythonAPI_PMR/openData/CrowdManagement.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def getCrowdManagementOfDayCharleroi(day):
+    # Example of validating the format of the day string
+    if len(day) != 6:
+        return {'error': 'Invalid day format. Please provide day in ddmmyy format.'}
+
+    # Read the CSV file
+    charleroi = pd.read_csv("pythonAPI_PMR/donnéesCharleroi.csv")
+
+    # Convert the 'timestamp' column to datetime with correct format
+    charleroi['timestamp'] = pd.to_datetime(charleroi['timestamp'], format='%d/%m/%Y %H:%M:%S')
+
+    # Convert day to the expected format (if needed)
+    day_str_formatted = day[:2] + '/' + day[2:4] + '/' + day[4:]
+
+    # Filter data for the specified day
+    charleroi_filtered = charleroi[charleroi['timestamp'].dt.strftime('%d/%m/%y') == day_str_formatted]
+
+    # Convert filtered data to JSON
+    json_data = charleroi_filtered.to_dict(orient='records')
+
+    # Return JSON response
+    return json_data
+
--- pythonAPI_PMR/views.py
+++ pythonAPI_PMR/views.py
@@ -52,24 +52,8 @@ def documentationTrain(request):
 
 
 def getCrowdManagementOfDayCharleroi(request, day):
-    # Example of validating the format of the day string
-    if len(day) != 6:
-        return JsonResponse({'error': 'Invalid day format. Please provide day in ddmmyy format.'})
-
-    # Read the CSV file
-    charleroi = pd.read_csv("pythonAPI_PMR/donnéesCharleroi.csv")
-
-    # Convert the 'timestamp' column to datetime with correct format
-    charleroi['timestamp'] = pd.to_datetime(charleroi['timestamp'], format='%d/%m/%Y %H:%M:%S')
-
-    # Convert day to the expected format (if needed)
-    day_str_formatted = day[:2] + '/' + day[2:4] + '/' + day[4:]
-
-    # Filter data for the specified day
-    charleroi_filtered = charleroi[charleroi['timestamp'].dt.strftime('%d/%m/%y') == day_str_formatted]
-
     # Convert filtered data to JSON
-    json_data = charleroi_filtered.to_dict(orient='records')
+    json_data = CrowdManagement.getCrowdManagementOfDayCharleroi(day)
 
     # Return JSON response
     return JsonResponse(json_data, safe=False)
