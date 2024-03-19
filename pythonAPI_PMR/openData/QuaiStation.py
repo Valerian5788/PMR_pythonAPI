4d729f066a4ad4d616afa39f31f7e2e3ad13a08a
valerian <valerianvdc.pro@gmail.com>
2024-03-19 16:11:06 +0100

    test tri quai4

--- pythonAPI_PMR/openData/QuaiStation.py
+++ pythonAPI_PMR/openData/QuaiStation.py
@@ -5,12 +5,15 @@ def get_hauteur_quai(station):
     url = f"https://opendata.infrabel.be/api/explore/v2.1/catalog/datasets/perronhoogten-in-stations/records?select=*&where=longnamefrench%20LIKE%20%27{station}%27&limit=99"
     response = requests.get(url)
     if response.status_code == 200:
-        json_data = {
-            "nom de la station: ", response["longnamefrench"],
-            "quai", response["quai"],
-            "Type de quai", response["platform_type"],
-            "Hauteur", response["hauteur"]
-        }
+        data = response.json()
+        for result in data["results"]:
+            json_data = {
+                "nom de la station: ", data["longnamefrench"],
+                "quai", data["quai"],
+                "Type de quai", data["platform_type"],
+                "Hauteur", data["hauteur"],
+                "Coordonnées", data["geometry"]
+            }
         return json_data
     else:
         return None
