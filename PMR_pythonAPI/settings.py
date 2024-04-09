42d8089dc5575edf42a1c56885936c3037398baf
Valerian5788 <valerianvdcpro@gmail.com>
2024-04-09 16:41:08 +0200

    change test2

--- PMR_pythonAPI/settings.py
+++ PMR_pythonAPI/settings.py
@@ -53,7 +53,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://example.com",
+    "http://example2.com",
+]
 
 ROOT_URLCONF = 'PMR_pythonAPI.urls'
 #tentative cors
