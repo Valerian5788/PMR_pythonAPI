85805c598ede85b42c4756bc44cdd385aa0dfb95
Valerian5788 <valerianvdcpro@gmail.com>
2024-04-09 17:20:00 +0200

    Tri supp dans les stations name et cela renvoie direct long / lat

--- pythonAPI_PMR/openData/stations_name.py
+++ pythonAPI_PMR/openData/stations_name.py
@@ -5,8 +5,16 @@ import pandas as pd
 def get_stations_name():
     csv_file_path = os.path.join(BASE_DIR, 'pythonAPI_PMR/internData/stations.csv')
     stations = pd.read_csv(csv_file_path)
-    #only have the name column
-    stations_name = stations['name'].tolist()
-    return stations_name
+
+    # Filter the DataFrame where 'country-code' is 'be'
+    stations = stations[stations['country-code'] == 'be']
+
+    # Extract the 'name', 'longitude', and 'latitude' columns
+    stations_info = stations[['name', 'longitude', 'latitude']]
+
+    # Convert the DataFrame to a list of dictionaries
+    stations_info_list = stations_info.to_dict('records')
+
+    return stations_info_list
 
 print(get_stations_name())
