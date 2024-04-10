bcf36d19aa282f29e003e5347d885da8925f0e69
Valerian5788 <valerianvdcpro@gmail.com>
2024-04-10 12:15:02 +0200

    get all facilities

--- pythonAPI_PMR/openData/facilities.py
+++ pythonAPI_PMR/openData/facilities.py
@@ -2,30 +2,24 @@ import json
 import os
 from PMR_pythonAPI.settings import BASE_DIR
 
+
 def get_facilities_data():
-    # Chemin vers le fichier facilities.json dans votre projet
     json_file_path = os.path.join(BASE_DIR, 'pythonAPI_PMR//internData/facilities.json')
 
-    # Vérifier si le fichier existe
     if os.path.exists(json_file_path):
-        # Ouvrir et lire le contenu du fichier JSON
         with open(json_file_path, 'r') as file:
             json_data = json.load(file)
 
-        # Filtrer les données pour obtenir seulement la station "Charleroi-Central" et "Namur"
-        charleroi_data = next((station for station in json_data if station["station"] == "Charleroi-Central"), None)
-        namur_data = next((station for station in json_data if station["station"] == "Namur"), None)
+        # Remove null parameters and sort the JSON data
+        for station in json_data:
+            station = {k: v for k, v in station.items() if v is not None}
 
-        if charleroi_data and namur_data:
-            # Créer un dictionnaire contenant les données des deux stations
-            response_data = {
-                "Charleroi-Central": charleroi_data["facilities"],
-                "Namur": namur_data["facilities"]
-            }
-            return response_data
-        else:
-            return print("Les données pour les stations 'Charleroi-Central' et 'Namur' n'ont pas été trouvées.")
+        sorted_json_data = json.dumps(json_data, sort_keys=True)
+
+        return sorted_json_data
     else:
         return print("Le fichier facilities.json n'existe pas.")
 
+
 print(get_facilities_data())
+
