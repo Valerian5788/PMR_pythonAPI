63f623a5b264aabd80b5052c72e20db85d586ced
Valerian5788 <valerianvdcpro@gmail.com>
2024-04-10 12:33:03 +0200

    no tri

--- pythonAPI_PMR/openData/facilities.py
+++ pythonAPI_PMR/openData/facilities.py
@@ -8,14 +8,7 @@ def get_facilities_data():
     if os.path.exists(json_file_path):
         with open(json_file_path, 'r') as file:
             json_data = json.load(file)
-
-        # Remove null parameters and sort the JSON data
-        for station in json_data:
-            station = {k: v for k, v in station.items() if v is not None}
-
-        sorted_json_data = json.dumps(json_data, sort_keys=True)
-
-        return sorted_json_data
+        return json_data
     else:
         return "Le fichier facilities.json n'existe pas."
 
