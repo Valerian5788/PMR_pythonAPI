79dbd4f3088e82eaee6d688aecd36d7a7bf594c2
valerian <valerianvdc.pro@gmail.com>
2024-04-17 12:12:47 +0200

    Add parse station name to get hauteur quai

--- pythonAPI_PMR/openData/QuaiStation.py
+++ pythonAPI_PMR/openData/QuaiStation.py
@@ -2,8 +2,11 @@ import requests
 
 
 def get_hauteur_quai(station):
-    url = f"https://opendata.infrabel.be/api/explore/v2.1/catalog/datasets/perronhoogten-in-stations/records?select=*&where=longnamefrench%20LIKE%20%27{station}%27&limit=99"
-    response = requests.get(url)
+    parsed_station = parse_station_name(station)
+
+    url_french = f"https://opendata.infrabel.be/api/explore/v2.1/catalog/datasets/perronhoogten-in-stations/records?select=*&where=longnamefrench%20LIKE%20%27{parsed_station}%27&limit=99"
+    url_dutch = f"https://opendata.infrabel.be/api/explore/v2.1/catalog/datasets/perronhoogten-in-stations/records?select=*&where=longnamedutch%20LIKE%20%27{parsed_station}%27&limit=99"
+    response = requests.get(url_french)
     if response.status_code == 200:
         data = response.json()["results"]
         extracted_data = []
@@ -18,7 +21,27 @@ def get_hauteur_quai(station):
 
         extracted_data.sort(key=lambda x: int(x["id du quai"]))
         return {"donnees quai : ": extracted_data}
-    else:
-        return None
+
+    elif response.status_code == 404:
+        response = requests.get(url_dutch)
+        if response.status_code == 200:
+            data = response.json()["results"]
+            extracted_data = []
+            for result in data:
+                station_data = {
+                    "nom de la gare": result["longnamedutch"],
+                    "id du quai": result["quai"],
+                    "hauteur": result["hoogte"],
+                    "coordonnées": result["geo_point_2d"]
+                }
+                extracted_data.append(station_data)
+
+            extracted_data.sort(key=lambda x: int(x["id du quai"]))
+            return {"donnees quai : ": extracted_data}
+        else:
+            return None
 
 
+
+def parse_station_name(station_name):
+    return station_name.split("/")[0]
